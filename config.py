ffbf9be7676ae331f9e25102809f2faf6804602b
Marko870 <mark12bolton@gmail.com>
2026-03-09 05:04:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     DATABASE_URL     = os.getenv("DATABASE_URL", "")
     USDT_ADDRESS     = os.getenv("USDT_ADDRESS", "0xd29c0f1d945a650b0c8158396682c56f586af13e")
     USDT_NETWORK     = os.getenv("USDT_NETWORK", "ERC20")
-    SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@ARTIZILLO)
+    SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@ARTIZILLO")
     BOT_NAME         = "🛍️ Nova Plus"
